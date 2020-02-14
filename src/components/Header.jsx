18f35f8f15f365d3c06524cabe9bec6a43852378
Judah Barton <judah.barton@yahoo.com>
2020-02-14 12:29:24 -0800

    add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,12 @@ import './Header.css';
 function Header(){
   return (
     <div>
+      
+    <div className='topBackground'>
+      <img className='logo' src="https://heathenbrewing.com/wp-content/uploads/2017/10/heathen-tiki.png" alt="logo"/>
       <Link to="/">Home</Link> | <Link to="/kegs">Kegs</Link> | <Link to="/newkeg">New Keg</Link> 
       <h1>TROY3OY BREWING - TROY3OY PUBLIC HOUSE</h1>
+    </div>
     </div>
   );
 }
