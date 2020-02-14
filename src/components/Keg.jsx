9067f9cd0ec68ba6e227850ad3b38f18abdf2cf4
Judah Barton <judah.barton@yahoo.com>
2020-02-14 11:45:38 -0800

    add some styling and more code

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props){
+    var kegStyles = {
+        marginBottom: '100px'
+    }
   return (
-    <div >
-        <h3>{props.name}</h3><h3>{props.type}</h3>
-        <p><em>{props.alcoholContent}</em></p>
+      <div>
+    <div style = {kegStyles}>
+        <img src={props.beerLabel} alt={props.name}></img>
+        <h3>{props.name}, <em>{props.type}</em></h3>
+        <p>{props.alcoholContent} ~ {props.brand}</p>
+        <p>{props.price}</p>
+    </div>
     </div>
   );
 }
@@ -15,7 +22,8 @@ Keg.propTypes = {
   alcoholContent: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired
+  price: PropTypes.string.isRequired,
+  beerLabel: PropTypes.string.isRequired
 };
 
 export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -7,35 +7,48 @@ var masterKegList =  [
     brand: 'TroyBoy',
     price:'$10.00',
     alcoholContent: '0.01%',
-    type:'Lager'
+    type:'Lager',
+    beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-687467_f22ab_sm.jpeg',
   },
   {
     name:'Chich',
     brand: 'TroyBoy',
     price:'$25.00',
     alcoholContent: '100%',
-    type:'Moon Shine'
+    type:'Moon Shine',
+    beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-1347185_550de_sm.jpeg',
   },
   {
     name:'Your my boy blue',
     brand: 'Brew Bus Brewing',
     price:'$5.00',
     alcoholContent: '9.4%',
-    type:'Porter'
+    type:'Porter',
+    beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-654408_1e75e_sm.jpeg',
   },
   {
     name:'Badlands Pilsner',
     brand: 'Heathen Brewing',
     price:'$8.00',
     alcoholContent: '5.2%',
-    type:'American Pilsner'
+    type:'American Pilsner',
+    beerLabel:'https://untappd.akamaized.net/site/brewery_logos/brewery-28019_86c28.jpeg',
   },
   {
     name:'Promiscuouse Blonde',
     brand: 'Heathen Brewing',
     price:'$5.50',
     alcoholContent: '5.5%',
-    type:'Blonde Ale'
+    type:'Blonde Ale',
+    beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-234300_ce47a_sm.jpeg',
+  },
+  {
+    name:'Wabbit Saison',
+    brand: 'Heathen Brewing',
+    price:'$11.50',
+    alcoholContent: '6.2%',
+    type: 'Belgian-Style Saison',
+    beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-3428585_3f464_sm.jpeg',
   }
 ];
 
@@ -49,6 +62,8 @@ function KegList(){
           brand={keg.brand}
           price={keg.price}
           alcoholContent={keg.alcoholContent}
+          type={keg.type}
+          beerLabel={keg.beerLabel}
           key={index}/>
       )}
     </div>
