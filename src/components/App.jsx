e96ec3c6f1d78b3bb7ad5f22d3cefbdbbafddba1
Judah Barton <judah.barton@yahoo.com>
2020-02-21 09:16:38 -0800

    update keg form and app as well as move masterKeglist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,20 +9,86 @@ import Home from './Home';
 import Error404 from './Error404';
 import './App.css';
 
-function App(){
-  return (
-    <div className='general'>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/kegs' component={KegList} />
-        <Route path='/newkeg' component={NewKegForm} />
-        <Route path='/signin' component={SignIn} />
-        <Route path='/signup' component={SignUp} />
-        <Route component={Error404}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      masterKegList [
+        {
+          name:'Trash',
+          brand: 'TroyBoy',
+          price:'$10.00',
+          alcoholContent: '0.01%',
+          type:'Lager',
+          beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-687467_f22ab_sm.jpeg',
+        },
+        {
+          name:'Chich',
+          brand: 'TroyBoy',
+          price:'$25.00',
+          alcoholContent: '100%',
+          type:'Moon Shine',
+          beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-1347185_550de_sm.jpeg',
+        },
+        {
+          name:'Your my boy blue',
+          brand: 'Brew Bus Brewing',
+          price:'$5.00',
+          alcoholContent: '9.4%',
+          type:'Porter',
+          beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-654408_1e75e_sm.jpeg',
+        },
+        {
+          name:'Badlands Pilsner',
+          brand: 'Heathen Brewing',
+          price:'$8.00',
+          alcoholContent: '5.2%',
+          type:'American Pilsner',
+          beerLabel:'https://untappd.akamaized.net/site/brewery_logos/brewery-28019_86c28.jpeg',
+        },
+        {
+          name:'Promiscuouse Blonde',
+          brand: 'Heathen Brewing',
+          price:'$5.50',
+          alcoholContent: '5.5%',
+          type:'Blonde Ale',
+          beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-234300_ce47a_sm.jpeg',
+        },
+        {
+          name:'Wabbit Saison',
+          brand: 'Heathen Brewing',
+          price:'$11.50',
+          alcoholContent: '6.2%',
+          type: 'Belgian-Style Saison',
+          beerLabel:'https://untappd.akamaized.net/site/beer_logos/beer-3428585_3f464_sm.jpeg',
+        }
+      ]
+    };
+    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
+  }
+
+  handleAddingNewKegToList(newKeg){
+    let newMasterKegForm = this.state.masterKegList.slice();
+    newMasterKegForm.push(newKeg);
+    this.setState({masterKegList: newMasterKegForm});
+  }
+
+  render(){
+    return (
+      <div className='general'>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/kegs' component={KegList} />
+          <Route path='/newkeg' component={NewKegForm} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route component={Error404}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
-function NewKegForm(){
+function NewKegForm(props){
+  let _name = null;
+  let _brand = null;
+  let _price = null;
+  let _alcoholContent = null;
+  let _type = null;
+  let _beerLabel = null;
+
+  function handleNewKegFormSubmission(event){
+    event.preventDefault();
+    props.onNewKegCreation({name: _name.value, brand: _brand.value, price: _price.value, alcoholContent: _alcoholContent.value, type: _type.value, beerLabel: _beerLabel.value, id:v4()});
+    _name.value = '';
+    _brand.value = '';
+    _price.value = '';
+    _alcoholContent.value = '';
+    _type.value = '';
+    _beerLabel.value = '';
+  }
+
   var NewKegFormStyles = {
     textAlign: "center",
     margin: "20px",
@@ -17,36 +37,44 @@ function NewKegForm(){
     borderRadius: '5px',
   }
 
-
-
   return (
     <div style = {NewKegFormStyles}>
         <h1> New Keg </h1>
-      <form>
+      <form onSubmit={handleNewKegFormSubmission}>
         <input style = {inputStyles}
           type='text'
           id='name'
           placeholder='Keg Name'/>
+          ref={(input) => {_name = input;}}/>
         <input style = {inputStyles}
           type='text'
           id='brand'
           placeholder='Brand'/>
+          ref={(input) => {_brand = input;}}/>
         <input style = {inputStyles}
           id='price'
           placeholder='Price'/>
+          ref={(input) => {_price = input;}}/>
           <input style = {inputStyles}
           id='alcoholContent'
           placeholder='ABV'/>
+          ref={(input) => {_alcoholContent = input;}}/>
           <input style = {inputStyles}
           id='type'
-          placeholder='Type'/> 
+          placeholder='Type'/>
+          ref={(input) => {_type = input;}}/>
           <input style = {inputStyles}
           id='beerLabel'
-          placeholder='Image URL'/><br></br>
+          placeholder='Image URL'/>
+          ref={(input) => {_beerLabel = input;}}/><br></br>
         <button type="submit" class="btn btn-outline-danger">Create</button>
       </form>
     </div>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
 
 export default NewKegForm;
