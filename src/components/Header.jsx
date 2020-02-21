84d007404fefe7f47c78f21e5ddc2fd614898d5a
Judah Barton <judah.barton@yahoo.com>
2020-02-21 15:58:24 -0800

    update the links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,12 @@ import './Header.css';
 function Header(){
   return (
     <div>
+    <div className='links'>
+    <Link to="/kegs">Kegs</Link> | <Link to="/newkeg">New Keg</Link>
+    </div>
     <div className='topBackground'>
-      <img className='logo' src="https://heathenbrewing.com/wp-content/uploads/2017/10/heathen-tiki.png" alt="logo"/>
-      <Link to="/">Home</Link> | <Link to="/kegs">Kegs</Link> | <Link to="/newkeg">New Keg</Link> | <Link to="/signin">Sign In</Link> | <Link to="/signup">Sign Up</Link>
-      <h1>TROY3OY BREWING - TROY3OY PUBLIC HOUSE</h1>
+    <Link to="/"><img className='logo' src="https://heathenbrewing.com/wp-content/uploads/2017/10/heathen-tiki.png" alt="logo"/></Link>
+    <h1>TROY3OY BREWING - TROY3OY PUBLIC HOUSE</h1>
     </div>
     </div>
   );
